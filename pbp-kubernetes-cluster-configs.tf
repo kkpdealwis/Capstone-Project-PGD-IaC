aefd3d143b3f1a73a3337c11043f96f14bf774d4
pubuduAeturnum <98076289+pubuduAeturnum@users.noreply.github.com>
2025-01-30 20:06:23 +0530

    update k8s security groups

--- pbp-kubernetes-cluster-configs.tf
+++ pbp-kubernetes-cluster-configs.tf
@@ -99,6 +99,12 @@ resource "aws_security_group" "k8s-master-node-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -162,6 +168,12 @@ resource "aws_security_group" "k8s-worker-node-sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
